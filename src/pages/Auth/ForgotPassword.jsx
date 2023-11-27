e9b49389789933bb9d2bb52c14f21970eebb8836
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-11-27 11:32:09 +0100

    fix bug in forgot password page

--- src/pages/Auth/ForgotPassword.jsx
+++ src/pages/Auth/ForgotPassword.jsx
@@ -3,11 +3,18 @@ import bodyBuddy from "../../assets/bodybuddy.png";
 import AuthContainer from "./components/AuthContainer";
 import Logo from "../../components/Logo";
 import Title from "../../components/Title";
-import FormContainer from "../../components/FormContainer";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import { useForm } from "react-hook-form";
 
 export default function ForgotPassword() {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <AuthContainer
       mainClassName="w-screen h-screen bg-alice-blue flex items-center justify-center"
@@ -18,13 +25,16 @@ export default function ForgotPassword() {
       <p className="text-slate-gray text-sm w-[350px] mb-5">
         An email with your password reset link will be emailed to you
       </p>
-      <FormContainer className="flex flex-col">
+
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
         <Input
           id="useremail"
           label="Email"
           type="email"
           placeholder="Enter your email"
           className="mb-3"
+          register={register}
+          required
         />
         <Button className="text-sm">Submit</Button>
         <Link
@@ -33,7 +43,7 @@ export default function ForgotPassword() {
         >
           Back to Login
         </Link>
-      </FormContainer>
+      </form>
     </AuthContainer>
   );
 }
